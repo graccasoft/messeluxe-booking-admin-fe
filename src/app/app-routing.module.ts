49553a192e2e83d4b115bfaed0bfee1f3d0d1f73
Gracious Mashasha <graccasoft263@gmail.com>
2024-04-10 18:57:21 +0200

    layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminPagesComponent } from './admin-pages/admin-pages.component';
+import { BookingsComponent } from './admin-pages/bookings/bookings.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {
+    path: 'admin', component: AdminPagesComponent,
+    canActivate: [],
+    children: [
+      { path: '', component: BookingsComponent },
+      { path: 'bookings', component: BookingsComponent },
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdminPagesComponent } from './admin-pages/admin-pages.component';
+import { BookingsComponent } from './admin-pages/bookings/bookings.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminPagesComponent,
+    BookingsComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
